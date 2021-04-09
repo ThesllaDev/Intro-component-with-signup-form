f4cd69c1ee241f91a1d36d481ecf7c2f7520eeab
ThesllaDev <theslladev@gmail.com>
2021-04-09 14:15:45 -0300

    Refactor: replaces the old code with new features

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,32 +1,59 @@
-// STORING USER VALUES
-
-const firstName = document.querySelector('#first-name').value;
-const lastName = document.querySelector('#last-name').value;
-const email = document.querySelector('#email').value;
-const password = document.querySelector('#password').value;
-
-// OBJECT WITH VALUES FROM REGISTRATION FIELDS
-let recordValue = {
-    firstName,
-    lastName,
-    email,
-    password
-};
-
+const form = document.querySelector('#form');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
 let msgError = " cannot be empty";
 
-function validateFill() { // VALIDATING TEXT FIELD AND PRESENTING ERROR MSG
-    if (!recordValue.firstName){
-        document.getElementById("errorFirstName").innerHTML = "First Name" + msgError;
-        document.getElementById("first-name").classList.add("icon-error");
-    } else if (!recordValue.lastName){
-        document.getElementById("errorLastName").innerHTML = "Last Name" + msgError;
-        document.getElementById("last-name").classList.add("icon-error");
-    } else if (!recordValue.email){
-        document.getElementById("errorEmail").innerHTML = "Looks like this is not an email";
-        document.getElementById("email").classList.add("icon-error");
-    } else if (!recordValue.password){
-        document.getElementById("errorPassword").innerHTML = "Password" + msgError;
-        document.getElementById("password").classList.add("icon-error");
+function errorFunc(element) {
+    element.classList.remove('valid')
+    element.classList.add('icon-error');
+}
+
+function correctFunc(element) {
+    element.classList.remove('icon-error')
+    element.classList.add('valid');
+}
+
+function validateFirstName() {
+    firstName.value.length < 1 ? (errorFunc(firstName),
+        document.querySelector('#errorFirstName').textContent = `First Name ${msgError}`)
+        : (correctFunc(firstName),
+            document.querySelector('#errorFirstName').textContent = "");
+}
+
+function validateLastName() {
+    lastName.value.length < 1 ? (errorFunc(lastName),
+        document.querySelector('#errorLastName').textContent = `Last Name ${msgError}`)
+        : (correctFunc(lastName),
+            document.querySelector('#errorLastName').textContent = "");
+}
+
+function validateEmail() {
+    let mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (email.value.match(mailFormat)) {
+        correctFunc(email)
+        document.querySelector('#errorEmail').textContent = ""
+    } else {
+        errorFunc(email)
+        document.querySelector('#errorEmail').textContent = `You must enter a valid email address`
     }
 }
+
+function validatePassword() {
+    password.value.length < 8 || password.value.length > 30 ? (errorFunc(password),
+        document.querySelector('#errorPassword').textContent = `
+    Your password must be between 8 and 30 characters`)
+        : (correctFunc(password),
+            document.querySelector('#errorPassword').textContent = "");
+}
+
+function validateForm() {
+    form.addEventListener('click', (error) => {
+        error.preventDefault();
+        validateFirstName();
+        validateLastName();
+        validateEmail();
+        validatePassword();
+    })
+};
